177019dfd32d97a5449e9d7e5f44926df6fd63c9
likitha9 <sivalikitha.valluru9@gmail.com>
2020-04-14 19:17:21 -0400

    Update main_hmm.py
    List of variables: T, N, M, Q, V, A, B, pi, O

--- main_hmm.py
+++ main_hmm.py
@@ -1,3 +1,12 @@
 """
-
+Constructing an HMM:
+T = length of observation sequence
+N = number of states in the model
+M = number of observation symbols
+Q = distinct states of the Markov process
+V = set of possible observations
+A = state transition probabilities
+B = observation probability matrix
+pi = initial state distribution
+O = observation sequence
 """
