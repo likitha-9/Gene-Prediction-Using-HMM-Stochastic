bb409097c5adf6d5e6241f6484e4ac8b15cafe70
likitha9 <sivalikitha.valluru9@gmail.com>
2020-04-14 21:15:25 -0400

    -- SAVE --

--- amino_dna-codon.py
+++ amino_dna-codon.py
@@ -10,12 +10,33 @@ train = open("./training_set.txt", "r")
 
 pairings = {}
 
+def remove_whitespace(line):
+      while '' in line:
+            line.remove('')
+      return line
+
 for i in obs.readlines():
       pairings[i[:-1]] = []   #the slicing is to remove any immediate delimiter (following the name)
 
+count=0
 while True:
-      line1 = train.readline().split(" ")
-      line2 = train.readline().split(" ")
-      whitespace = train.readline() #discarded
+      try:
+            line1 = train.readline().split(" ") #DNA codons
+            line2 = train.readline().split(" ") #amino acids
+            whitespace = train.readline() #discarded
 
-      print(line1, line2)
+            line1 = remove_whitespace(line1)
+            line2 = remove_whitespace(line2)
+            
+            for i in range(0,len(line2)):
+                  try:
+                        if line2[i] in pairings:
+                              if line1[i] not in pairings[line2[i]]:
+                                    pairings[line2[i]].append(line1[i])
+                  except:
+                        break
+            count+=1
+            print(count)
+      except:
+            break
+print(pairings)
