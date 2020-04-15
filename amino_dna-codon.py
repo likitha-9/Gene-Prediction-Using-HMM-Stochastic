d5c5cbb8c1d7933bf3d130aafe03067e730f5da7
likitha9 <sivalikitha.valluru9@gmail.com>
2020-04-14 20:49:30 -0400

    Update amino_dna-codon.py
    Read lines in pairs

--- amino_dna-codon.py
+++ amino_dna-codon.py
@@ -1,3 +1,21 @@
 """
 A dictionary of {amino_acid: {list of DNA codons}}
 """
+
+import os
+
+#open needed files
+obs = open("./observations.txt", "r")
+train = open("./training_set.txt", "r")
+
+pairings = {}
+
+for i in obs.readlines():
+      pairings[i[:-1]] = []   #the slicing is to remove any immediate delimiter (following the name)
+
+while True:
+      line1 = train.readline().split(" ")
+      line2 = train.readline().split(" ")
+      whitespace = train.readline() #discarded
+
+      print(line1, line2)
