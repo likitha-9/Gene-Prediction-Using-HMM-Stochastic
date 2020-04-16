3da91c5bdc2fafc18e44bbe34e942516e64779c7
likitha9 <sivalikitha.valluru9@gmail.com>
2020-04-15 20:19:09 -0400

    Update dna_sequence.py

--- dna_sequence.py
+++ dna_sequence.py
@@ -3,4 +3,25 @@ Merge all of the DNA codons into a single sequence ==> a string excluding NNN
 """
 
 def compile_dna_sequence():
+      dna = ""
+      file = open("./training_set.txt", "r")
+      index=0
+      for line in file.readlines():
+            if index%3==0:
+                  split = line.split(" ")
+                  for i in split:
+                        if i!="NNN" and check_type(i):
+                              dna += i
+            index+=1
+      return dna
+
+def check_type(i):
+      try:
+            type(int(i))
+            return False      #the integer at the end of the file isn't needed. So, return False if the type(i)=int
+      except:
+            return True
+            
+dna = compile_dna_sequence()
+      
       
