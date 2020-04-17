9d32e7aaf350103e83862b70399f32ed66f9ce83
likitha9 <sivalikitha.valluru9@gmail.com>
2020-04-17 19:30:35 -0400

    Update initial_dna_transitions.py

--- initial_dna_transitions.py
+++ initial_dna_transitions.py
@@ -16,11 +16,15 @@ import initial_dna_sequence
 
 def probs(sequence):
       #initial dictionaries
-      a = {'c':0, 'g':0, 't':0 }
-      c = {'a':0, 'g':0, 't':0 }
-      g = {'c':0, 'a':0, 't':0 }
-      t = {'c':0, 'g':0, 'a':0 }
-      print(sequence)
+      diction = { 'a' : {'c':0, 'g':0, 't':0 },
+                  'c' : {'a':0, 'g':0, 't':0 },
+                  'g' : {'c':0, 'a':0, 't':0 },
+                  't' : {'c':0, 'g':0, 'a':0 },
+            }
+      for i in range(0,len(sequence)-1):
+            diction[sequence[i].lower()][sequence[i+1].lower()]+=1
+            
+      print(diction)
       return 0
       
 
