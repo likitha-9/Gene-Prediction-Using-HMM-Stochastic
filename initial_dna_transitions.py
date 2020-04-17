43d7ee3093f7a173ae891641392c305886297e1f
likitha9 <sivalikitha.valluru9@gmail.com>
2020-04-17 19:38:17 -0400

    Update initial_dna_transitions.py
    counts computed

--- initial_dna_transitions.py
+++ initial_dna_transitions.py
@@ -16,16 +16,18 @@ import initial_dna_sequence
 
 def probs(sequence):
       #initial dictionaries
-      diction = { 'a' : {'c':0, 'g':0, 't':0 },
-                  'c' : {'a':0, 'g':0, 't':0 },
-                  'g' : {'c':0, 'a':0, 't':0 },
-                  't' : {'c':0, 'g':0, 'a':0 },
-            }
+      diction = { 'a' : {'a':0, 'c':0, 'g':0, 't':0 },
+                  'c' : {'a':0, 'c':0, 'g':0, 't':0 },
+                  'g' : {'a':0, 'c':0, 'g':0, 't':0 },
+                  't' : {'a':0, 'c':0, 'g':0, 't':0 } }
       for i in range(0,len(sequence)-1):
-            diction[sequence[i].lower()][sequence[i+1].lower()]+=1
+            try:
+                  diction[sequence[i].lower()][sequence[i+1].lower()]+=1
+            except:
+                  print(i,sequence[i])
             
       print(diction)
-      return 0
+      return diction
       
 
 dna = initial_dna_sequence.compile_dna_sequence()
