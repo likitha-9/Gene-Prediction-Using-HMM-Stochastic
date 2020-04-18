3879477e28445f1cb919130179c812ba5040f124
likitha9 <sivalikitha.valluru9@gmail.com>
2020-04-17 21:43:16 -0400

    Update initial_dna_transitions.py

--- initial_dna_transitions.py
+++ initial_dna_transitions.py
@@ -20,18 +20,20 @@ def probs(sequence):
                   'c' : {'a':0, 'c':0, 'g':0, 't':0 },
                   'g' : {'a':0, 'c':0, 'g':0, 't':0 },
                   't' : {'a':0, 'c':0, 'g':0, 't':0 } }
-      for i in range(0,len(sequence)-1):
+      for i in range(0,len(sequence)):
             try:
                   diction[sequence[i].lower()][sequence[i+1].lower()]+=1
             except:
-                  print(i,sequence[i])
+                  if(i==len(sequence)-1):
+                        pass        #last character of the DNA sequence is '\n'.
+                  else:
+                        print(i,sequence[i])
             
       print(diction)
       return diction
       
-
 dna = initial_dna_sequence.compile_dna_sequence()
-matrix = probs(dna)
+probs()
 
 
 
