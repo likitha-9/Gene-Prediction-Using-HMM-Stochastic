70ca80c292f23c2c921aa293984705387baa6832
likitha9 <sivalikitha.valluru9@gmail.com>
2020-04-17 21:52:09 -0400

    Update initial_dna_transitions.py

--- initial_dna_transitions.py
+++ initial_dna_transitions.py
@@ -20,21 +20,29 @@ def probs(sequence):
                   'c' : {'a':0, 'c':0, 'g':0, 't':0 },
                   'g' : {'a':0, 'c':0, 'g':0, 't':0 },
                   't' : {'a':0, 'c':0, 'g':0, 't':0 } }
-      for i in range(0,len(sequence)):
+      for i in range(0,len(sequence)-1):  #last character of the DNA sequence (STRING) is '\n', so it's omitted
             try:
                   diction[sequence[i].lower()][sequence[i+1].lower()]+=1
             except:
-                  if(i==len(sequence)-1):
-                        pass        #last character of the DNA sequence is '\n'.
+                  if(i==len(sequence)-2):
+                        pass  #second to last character (STRING) doesn't play a role in the transition
                   else:
-                        print(i,sequence[i])
+                        print(i)
             
       print(diction)
       return diction
+
+def compute_probabilities(diction):
+      for key in diction:
+            count = 0
+            for each_value in diction[key]:
+                  count += each_value
+                  
       
 dna = initial_dna_sequence.compile_dna_sequence()
-probs()
-
+diction = probs(dna)    #dictionary of probabilities
+transition_probs = compute_probabilities(diction)
+print(transition_probs)
 
 
       
