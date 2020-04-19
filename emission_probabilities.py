e94a2a51ddd138d6b3fabbcfefa665d8aad83816
likitha9 <sivalikitha.valluru9@gmail.com>
2020-04-19 00:58:11 -0400

    Update emission_probabilities.py

--- emission_probabilities.py
+++ emission_probabilities.py
@@ -3,3 +3,16 @@ Initial Emission Probabilities
 
 Transitions from hidden states to observations (amino acids)
 """
+import initial_amino_sequence
+amino = initial_amino_sequence.amino
+
+hidden = ["a","c","g","t"]
+
+file = open("./observations.txt",'r')
+obs = []
+for line in file.readlines():
+      obs.append(line[:-1])   #'\n' not needed
+print(obs)
+
+#file = open("./observations.txt","r")
+#amino 
