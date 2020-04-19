beb821acbdbdc8a51c65a93569496ce57fed0f1b
likitha9 <sivalikitha.valluru9@gmail.com>
2020-04-19 01:07:55 -0400

    Update emission_probabilities.py

--- emission_probabilities.py
+++ emission_probabilities.py
@@ -12,3 +12,24 @@ file = open("./observations.txt",'r')
 obs = []
 for line in file.readlines():
       obs.append(line[:-1])   #'\n' not needed
+
+diction = {}
+for i in hidden:
+      diction[i] = {}
+      for state in sorted(obs):
+            diction[i][state]=0
+      
+def compute_emissions(diction,amino):
+      for i in range(0,len(amino)-1):
+            diction[amino[i][0].lower()][amino[i+1]] += 1  #computed counts
+      for i in diction:
+            count=0
+            for j in diction[i]:
+                  count += diction[i][j]
+            for j in diction[i]:
+                  diction[i][j] /= count
+      return diction
+
+emissions = compute_emissions(diction,amino)
+      
+            
