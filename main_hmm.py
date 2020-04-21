143f45d9053793c9e5190d92d013cc14a66226b4
likitha9 <sivalikitha.valluru9@gmail.com>
2020-04-20 22:34:43 -0400

    Update main_hmm.py

--- main_hmm.py
+++ main_hmm.py
@@ -14,9 +14,9 @@ T - (multiple)
 N - 4 (number of hidden states)
 M - 21 (number of unique amino acids, including Stop Codon (*)) - check observations.txt 
 Q - [A, C, G, T]
-V - [*, Ala, Arg, Asn, Asp, Cys, Gln, Glu, Gly, His, Ile, Leu, Lys, Met, Phe, Pro, Ser, Thr, Trp, Tyr, Val]
+V - import from the file ./data/observations.txt - [*, Ala, Arg, Asn, Asp, Cys, Gln, Glu, Gly, His, Ile, Leu, Lys, Met, Phe, Pro, Ser, Thr, Trp, Tyr, Val]
 A - import from the file hidden_state_transitions.py
-B - import from the file emission_probabilities
+B - import from the file emission_probabilities.py
 pi - start --> [0.25, 0.25, 0.25, 0.25]
 O - (multiple)
 
