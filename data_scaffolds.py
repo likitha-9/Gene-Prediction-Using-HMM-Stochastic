e6562e2f7e1f4bcd66c79308f4afd2bb2b00dcd2
likitha9 <sivalikitha.valluru9@gmail.com>
2020-04-27 15:20:29 -0400

    Create data_scaffolds.py

---
+++ data_scaffolds.py
@@ -0,0 +1,23 @@
+from csv import reader
+
+file = reader(open("./data/unplaced_sequences_126.csv",'r'))
+data = [[]]
+for row in file:
+    for i in row:
+        if 'NT_' not in i:  #first column of the csv. Because the CSV is messy/jagged, all the elements will be split with respect to an element that starts with 'NT_' within in
+            data[-1].append(i)
+        else:
+            data.append([])
+            data[-1].append(i)
+            
+data.remove(data[0])
+
+unplaced = []
+
+for i in range(0,len(data)):
+    while '' in data[i]:
+        data[i].remove('')
+    string=''
+    for j in range(1,len(data[i])):
+        string += data[i][j]
+    data[i] = [ data[i][0], string ]
